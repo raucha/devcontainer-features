dacbcb5efd197109be225455d1f675bf708a0945
raucha <hoge@hoge>
2023-09-24 04:24:17 +0900

    add pytorch

---
+++ src/pytorch/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+# check_packages() {
+#     if ! dpkg -s "$@" > /dev/null 2>&1; then
+#         if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+#             echo "Running apt-get update..."
+#             apt-get update -y
+#         fi
+#         apt-get -y install --no-install-recommends "$@"
+#     fi
+# }
+
+echo "Activating feature 'pytorch'"
+
+pip install pytorch
+
+echo "pytorch installed! "
